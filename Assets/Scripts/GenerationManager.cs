cd90281f84a0f75d524ce7ea80828e140c4b84e0
Carl Eriksson <carl.erikssonn@hotmail.com>
2023-07-10 15:06:51 +0200

    DEV-1: More detailed map

--- Assets/Scripts/GenerationManager.cs
+++ Assets/Scripts/GenerationManager.cs
@@ -5,15 +5,16 @@ using Vector3 = UnityEngine.Vector3;
 public class GenerationManager : MonoBehaviour
 {
     [SerializeField] private Transform mapParent = null;
+    [SerializeField] private GameObject tile = null;
     [SerializeField] private Material[] terrainMaterials = null;
     
     private MapManager mapManager = null;
     private void Start()
     {
         NullChecker();
-        GenerateMapMesh();
+        GenerateGroundMesh();
+        MergeMapMesh();
     }
-
 
     private void NullChecker()
     {
@@ -32,11 +33,64 @@ public class GenerationManager : MonoBehaviour
         }
     }
 
+    private void MergeMapMesh()
+    {
+        List<MeshRenderer> allMeshes = new List<MeshRenderer>();
+        if (mapParent.childCount > 0) {
+            for (int i = mapParent.childCount - 1; i >= 0; i--) {
+                allMeshes.Add(mapParent.GetChild(i).GetComponent<MeshRenderer>());
+            }
+        }
+
+        Mesh finalMesh = new Mesh();
+        CombineInstance[] combineInstance = new CombineInstance[allMeshes.Count];
+        for (int i = 0; i < allMeshes.Count; i++) {
+            combineInstance[i].mesh = allMeshes[i].GetComponent<MeshFilter>().sharedMesh;
+            combineInstance[i].transform = allMeshes[i].transform.localToWorldMatrix;
+        }
+
+        finalMesh.CombineMeshes(combineInstance);
+        GameObject newObject = new GameObject("mesh");
+        MeshFilter filter = newObject.AddComponent<MeshFilter>();
+        filter.sharedMesh = finalMesh;
+        MeshRenderer renderer = newObject.AddComponent<MeshRenderer>();
+        MeshCollider collider = newObject.AddComponent<MeshCollider>();
+
+        for (int i = mapParent.childCount - 1; i >= 0; i--) {
+            Transform child = mapParent.GetChild(i);
+            DestroyImmediate(child.gameObject);
+        }
+
+        newObject.transform.SetParent(mapParent, false);
+    }
+
+    private void GenerateGroundMesh() {
+        if (mapManager.Map.GetLength(0) == 0 || mapManager.Map.GetLength(1) == 0) return;
+        int sizeX = mapManager.Map.GetLength(0);
+        int sizeY = mapManager.Map.GetLength(1);
+        
+        for (int x = 0; x < sizeX; x++) {
+            for (int y = 0; y < sizeY; y++) {
+                GameObject newTile = Instantiate(tile, mapParent, false);
+                newTile.transform.position = new Vector3(x, 0, y);
+                newTile.transform.eulerAngles = new Vector3(-90, 0, 0);
+
+                if (y > 25)
+                {
+                    mapManager.Map[x, y].tileType = Tile.TileType.RIVER;
+                }
+
+                newTile.GetComponent<MeshRenderer>().material =
+                    mapManager.Map[x, y].tileType == Tile.TileType.DEFAULT ? terrainMaterials[0] : terrainMaterials[1];
+            }
+        }
+    }
+    
     private void GenerateMapMesh()
     {
-        if (mapManager.MapSize.x == 0 || mapManager.MapSize.y == 0) return;
-        int sizeX = mapManager.MapSize.x;
-        int sizeY = mapManager.MapSize.y;
+        if (mapManager.Map.GetLength(0) == 0 || mapManager.Map.GetLength(1) == 0) return;
+        int sizeX = mapManager.Map.GetLength(0);
+        int sizeY = mapManager.Map.GetLength(1);
 
         GameObject meshObject = new GameObject("terrainMesh");
         meshObject.transform.eulerAngles = new Vector3(180f, 0, 0);
@@ -56,10 +110,10 @@ public class GenerationManager : MonoBehaviour
             for (int y = 0; y < sizeY; y++)
             {
                 Vector3[] currentFace = {
-                    new Vector3(0 + x, 0, 0 + y),
-                    new Vector3(1 + x, 0, 0 + y),
+                    new Vector3(x, 0, y),
+                    new Vector3(1 + x, 0, y),
                     new Vector3(1 + x, 0, 1 + y),
-                    new Vector3(0 + x, 0, 1 + y)
+                    new Vector3(x, 0, 1 + y)
                 };
                 
                 vertices.Add(currentFace[0]);
--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -1,21 +1,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Pool;
 using Vector3Int = UnityEngine.Vector3Int;
+
+[System.Serializable]
+public class Tile
+{
+    public enum TileType
+    {
+        DEFAULT,
+        RIVER
+    };
+
+    public bool occupied;
+    public Vector3Int position;
+    public TileType tileType;
+
+    public Tile(bool occupied, Vector3Int position, TileType tileType)
+    {
+        this.occupied = occupied;
+        this.position = position;
+        this.tileType = tileType;
+    }
+}
 
 public class MapManager : MonoBehaviour
 {
     [SerializeField] private Vector2Int mapSize = Vector2Int.zero;
 
     public static MapManager Instance { get; private set; }
-    
-    private readonly List<Vector3Int> map = new List<Vector3Int>();
-    
-    public Vector2Int MapSize
-    {
-        get => mapSize;
-    }
-    
+    public Tile[,] Map { get; private set; } = null;
+
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -26,11 +42,36 @@ public class MapManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+        CreateMap();
+    }
+
+    private void CreateMap()
+    {
+        Map = new Tile[mapSize.x, mapSize.y];
+
+        for (int x = 0; x < Map.GetLength(0); x++)
+        {
+            for (int y = 0; y < Map.GetLength(1); y++)
+            {
+                Map[x, y] = new Tile(false, new Vector3Int(x, 0, y), Tile.TileType.DEFAULT);
+            }
+        }
     }
 
     public bool IsObstructed(Vector3Int position)
     {
-        return map.Contains(position);
+        for (int x = 0; x < Map.GetLength(0); x++)
+        {
+            for (int y = 0; y < Map.GetLength(1); y++)
+            {
+                if (Map[x, y].position == position)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
     private bool OutsideMapBounds(Vector3Int position)
@@ -39,31 +80,59 @@ public class MapManager : MonoBehaviour
                position.z < -mapSize.y || position.z > mapSize.y;
     }
 
-    public void AddPositions(List<Vector3Int> positions)
+    public void OccupyPositions(List<Vector3Int> positions)
     {
         if (!positions.Any())
         {
             throw new System.Exception("Tried to add zero new positions to the map");
         }
-        foreach (Vector3Int t in positions.Where(t => !map.Contains(t) && !OutsideMapBounds(t)))
+
+        foreach (Vector3Int pos in positions)
         {
-            map.Add(t);
+            bool foundPosition = false;
+            
+            for (int x = 0; x < Map.GetLength(0); x++)
+            {
+                for (int y = 0; y < Map.GetLength(1); y++)
+                {
+                    if (Map[x, y].position != pos) continue;
+                    foundPosition = true;
+                    Map[x, y].occupied = true;
+                }
+            }
+
+            if (!foundPosition)
+            {
+                throw new System.Exception("Tried to occupy a position that is not present in the map grid");
+            }
         }
     }
 
-    public void RemovePositions(List<Vector3Int> positions)
+    public void UnOccupyPositions(List<Vector3Int> positions)
     {
         if (!positions.Any())
         {
             throw new System.Exception("Tried to remove zero new positions in " + name);
         }
-        foreach (Vector3Int t in positions)
+
+        foreach (Vector3Int pos in positions)
         {
-            if (!map.Contains(t) || OutsideMapBounds(t))
+            bool foundPosition = false;
+            
+            for (int x = 0; x < Map.GetLength(0); x++)
             {
-                throw new System.Exception("Tried to remove a non existing position in " + name);
+                for (int y = 0; y < Map.GetLength(1); y++)
+                {
+                    if (Map[x, y].position != pos) continue;
+                    foundPosition = true;
+                    Map[x, y].occupied = false;
+                }
             }
-            map.Remove(t);
+
+            if (!foundPosition)
+            {
+                throw new System.Exception("Tried to unoccupy a position that is not present in the map grid");
+            }
         }
     }
 }
