5faeee9207c6ccec92a3f4f486b294a569a1e658
Carl Eriksson <carl.erikssonn@hotmail.com>
2023-07-12 12:08:46 +0200

    DEV-3: Added resource manager

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -1,6 +1,33 @@
 using UnityEngine;
 
+public struct ResourceClass
+{
+    private int food;
+    private int wood;
+    private int stone;
+    private int tin;
+    private int copper;
+    
+    public ResourceClass(int food, int wood, int stone, int tin, int copper)
+    {
+        this.food = food;
+        this.wood = wood;
+        this.stone = stone;
+        this.tin = tin;
+        this.copper = copper;
+    }
+    
+    public void ChangeResources(ResourceClass changeAmount)
+    {
+        food += changeAmount.food;
+        wood += changeAmount.wood;
+        stone += changeAmount.stone;
+        tin += changeAmount.tin;
+        copper += changeAmount.copper;
+    }
+}
+
 public class ResourceManager : MonoBehaviour
 {
-    
+    public ResourceClass CurrentResources { get; } = new ResourceClass();
 }
